0e6548929cd7eebd9d511f2dba99742c7f273f31
谷口陽斗 <151001421+taniguchi-haruto@users.noreply.github.com>
2024-01-19 15:12:12 +0900

    Create mondai.js

---
+++ mondai.js
@@ -0,0 +1,4 @@
+'use strict'
+
+const mondai1 = "変数 circleCircumference と circleArea を宣言して、円の円周と面積を計算した値を代入し出力してみましょう。あなたが予想した答えとは若干異なるかもしれませんが、心配しないでください。結果が予想した値とほぼ同じであれば正解です。"
+const ko_do1 = "const circleDiameter = 10;<br>// ここにあなたのコードを書いてください<br>console.log(circleCircumference);//円の円周" + "31.41592653589793" + " に近い値を表示<br>console.log(circleArea)//円の面積" + "78.53981633974483" + " に近い値を表示";
