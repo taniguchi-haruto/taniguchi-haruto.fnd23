b3fd0a0bb95433331d2991b8fd7d3959490e7cc8
谷口陽斗 <151001421+taniguchi-haruto@users.noreply.github.com>
2024-01-22 10:31:45 +0900

    Create pass.js

---
+++ pass.js
@@ -0,0 +1,30 @@
+'use strict'
+const url = "C:/Users/1598092/Desktop/DIG/DIG使用した画像/FND-23期.png"
+function passConfirmation() {
+    const pass1 = document.getElementById("pass1").value;
+    const pass2 = document.getElementById("pass2").value;
+    const pass3 = document.getElementById("pass3").value;
+    if (pass1 === "D" && pass2 === "I" && pass3 === "G") {
+        const h1 = document.getElementsByTagName("h1");
+        const text = document.getElementsByClassName("text");
+        const div = document.getElementsByTagName("div");
+        const body = document.getElementsByTagName("body")
+        h1[0].outerText = "";
+        text[0].style.display = "none"
+        text[1].style.display = "none"
+        text[2].style.display = "none"
+        body[0].style.width = "900px";
+        body[0].style.height = "400px"
+        div[0].style.backgroundImage = "url(" + url + ")";
+        div[0].style.width = "100%";
+        div[0].style.height = "100%"
+        div[0].style.display = "inline-block";
+        div[1].outerHTML = "<div>20日間ありがとうございました！<div>";
+        div[1].style.height = "100px"
+        div[1].style.fontSize = "55px"
+        div[1].style.textAlign = "center";
+
+    }
+}
+
+
